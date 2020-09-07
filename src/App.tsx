0ed715ffe19cf7ebbacb707155cc4baa5d0b0507
david11yf29 <david11yf29@gmail.com>
2020-09-07 19:29:07 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,16 @@ const App: React.FC = () => {
     console.log(text);
   }
 
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    })
+  }
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
